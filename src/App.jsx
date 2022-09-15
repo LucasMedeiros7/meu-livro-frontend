3db37b3737bde2a933512eaebcbf0912d8378e2b
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-09-15 01:00:41 -0300

    editando global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { Banner } from "./components/banner/Banner";
+
 import "./global.css";
 
 export function App() {
   return (
     <div>
-      <header>
-        <h1>Meu Livro</h1>
-      </header>
-
       <Banner />
     </div>
   );
