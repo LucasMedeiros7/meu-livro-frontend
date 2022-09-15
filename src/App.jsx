fa98e6184048f31cf276db40d91b9a5271e926ee
Kamirii <camilyalbres@gmail.com>
2022-09-15 20:10:28 -0300

    footer pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Banner } from "./components/banner/Banner";
 import { CardProduto } from "./components/cardProduto/CardProduto";
-
+import { Footer } from "./components/footer/footer"
 import "./global.css";
 
 export function App() {
   return (
     <div>
       <Banner />
-      <CardProduto/>
+      <CardProduto />
+      <Footer />
     </div>
   );
 }
