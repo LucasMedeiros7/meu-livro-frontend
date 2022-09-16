09acc4f2976887db8622375a08ae43d36794f340
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-16 17:14:48 -0300

    Inserindo componente Header no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Header } from "./components/Header";
 import { Banner } from "./components/banner/Banner";
 import { CardProduto } from "./components/cardProduto/CardProduto";
+
 
 import "./global.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Banner />
-      <CardProduto/>
+      <CardProduto />
     </div>
   );
 }
