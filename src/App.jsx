7568c78a02a5ce34d8f46f0d5a9a632eee2c32f6
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-09-16 20:46:43 -0300

    section com editoras pronta

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Banner } from "./components/banner/Banner";
 import { CardProduto } from "./components/cardProduto/CardProduto";
+import { Editoras } from "./components/Section/Editoras";
 import { Vantagens } from "./components/Section/Vantagens";
 
 import "./global.css";
@@ -9,8 +10,9 @@ export function App() {
   return (
     <div>
       <Banner />
-      <Vantagens/>
-      <CardProduto/>
+      <Vantagens />
+      <CardProduto />
+      <Editoras />
     </div>
   );
 }
---
+++ src/components/Section/Editoras.jsx
@@ -0,0 +1,23 @@
+import styles from "./editora.module.css"
+
+export const Editoras = () => {
+    return (
+        <section className={styles.section}>
+            <h3 className={styles.titulo}>As melhores editoras você encontra aqui!</h3>
+            <div className={styles.container}>
+                <span>
+                    <img className={styles.tamanho} src="src/assets/editora-5.jpg" alt="logo da Editora Ciranda Cultural" />
+                </span>
+                <span>
+                    <img className={styles.tamanho} src="src/assets/editora-2.png" alt="logo da Editora Grupo Autêntica" />
+                </span>
+                <span>
+                    <img className={styles.tamanho} src="src/assets/editora-3.png" alt="logo da Editora Pensamento" />
+                </span>
+                <span>
+                    <img className={styles.tamanho} src="src/assets/editora-4.png" alt="logo da Editora Pipoca e Nanquim" />
+                </span>
+            </div>
+        </section>
+    )
+}
