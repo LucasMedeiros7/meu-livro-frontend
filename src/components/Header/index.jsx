c76c11573f2c5b5d6e8700ddd3384699fd345316
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-16 17:02:38 -0300

    Estrutura HTML do componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import S from "./header.module.css"
+import carrinho_icon from '../../assets/carrinho_icon.png'
+import usuario_icon from '../../assets/usuario_icon.png'
+
+
+export function Header() {
+    return (
+        <header className={S.box_header}>
+
+            <h1>Meu livro</h1>
+            <div className={S.box_input}>
+                <input type="text" />
+                <button className={S.lupinha_botao} type="submit" />
+            </div>
+
+            <div className={S.box_icons}>
+                <img className={S.usuario_icon} src={usuario_icon} alt="icone do usuario" />
+                <p className={S.texto_usuario}> entre ou cadastre-se </p>
+                <img className={S.carrinho_icon} src={carrinho_icon} alt="icone do carrinho" />
+            </div>
+        </header>
+    )
+
+}
