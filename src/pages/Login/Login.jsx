7efb21435699c7e1bc268b99bb76636d325b44e5
Isadora Áraújo <isadoraraujo13@gmail.com>
2022-09-18 14:58:29 -0300

    evento de submit e service de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,14 +2,18 @@ import { Link } from "react-router-dom";
 import styles from "./login.module.css";
 
 export const Login = () => {
+    function evento(event) {
+        event.preventDefault()
+    }
+
     return (
         <main className={styles.main}>
             <h2>Faça seu login</h2>
             <p className={styles.paragrafo}>Ficamos felizes em ter você por aqui. Entre para prosseguir e tenha acesso a diversos livros e obtenha uma experiência de compra incrível!</p>
 
-            <form className={styles.formulario}>
-                <input className={styles.campo} type="text" placeholder="Digite seu email cadastrado" />
-                <input className={styles.campo} type="text" placeholder="Digite sua senha cadastrada" />
+            <form onSubmit={evento} className={styles.formulario}>
+                <input className={styles.campo} type="email" placeholder="Digite seu email" />
+                <input className={styles.campo} type="password" placeholder="Digite sua senha" />
                 <button className={styles.enviar} type="submit">Entrar</button>
             </form>
 
