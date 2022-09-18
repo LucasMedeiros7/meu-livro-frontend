aedcae523eec03a6a7592160e397a04162c5b87e
Lucasmedeiros7 <lucaasmedeiros7@gmail.com>
2022-09-17 22:05:59 -0300

    Estruturação e estilização dos itens do carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,14 @@ import { CardProduto } from "./components/cardProduto/CardProduto";
 import { Footer } from "./components/footer/footer";
 
 import "./global.css";
-<<<<<<< HEAD
 import { Carrinho } from "./pages/carrinho/Carrinho";
-=======
 import { Cadastro } from "./pages/cadastro/Cadastro";
->>>>>>> e6faf1ef480d0868cecd854e6b0321f0d4460d27
 
 export function App() {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
       <Carrinho />
-=======
-      <Cadastro />
->>>>>>> e6faf1ef480d0868cecd854e6b0321f0d4460d27
       <Footer />
     </div>
   );
--- src/pages/carrinho/Carrinho.jsx
+++ src/pages/carrinho/Carrinho.jsx
@@ -16,18 +16,45 @@ export function Carrinho() {
       ano_publicacao: "2017",
       autor: "J.K. Rowling",
     },
+    {
+      id_livro: 23,
+      titulo: "O poder do agora: Um guia para a iluminação espiritual",
+      descricao:
+        "Nós passamos a maior parte de nossas vidas pensando no passado e fazendo planos para o futuro. Ignoramos ou negamos o presente e adiamos nossas conquistas para algum dia distante, quando conseguiremos tudo o que desejamos e seremos, finalmente, felizes. Mas, se queremos realmente mudar nossas vidas, precisamos começar neste momento. Essa é a mensagem simples, mas transformadora de Eckhart Tolle: viver no Agora é o melhor caminho para a felicidade e a iluminação.Para compreender a jornada em direção a O Poder do Agora, a primeira coisa a fazer é deixar para trás nossa mente analítica e nosso falso ego. Desde o primeiro capítulo subiremos rapidamente para uma altitude mais elevada, onde se respira um ar mais leve, o ar espiritual. Embora seja uma travessia desafiadora, Eckhart Tolle nos guia com sua linguagem simples e facilita o entendimento de conceitos complexos ao escrever no formato de perguntas e respostas.Combinando conceitos do cristianismo, do budismo, do hinduísmo, do taoísmo e de outras tradições espirituais, Tolle elaborou um guia de grande eficiência para a descoberta do nosso potencial interior.Esse livro é um manual prático que nos ensina a tomar consciência dos pensamentos e das emoções que nos impedem de vivenciar plenamente a alegria e a paz que estão dentro de nós mesmos.",
+      categoria: "autoconhecimento, desenvolvimento pessoal",
+      url_img:
+        "https://images-na.ssl-images-amazon.com/images/I/71ef0B1lsrL.jpg",
+      preco: "31,92",
+      total_paginas: 224,
+      ano_publicacao: "2004",
+      autor: "Eckhart Tolle",
+    },
+    {
+      id_livro: 26,
+      titulo: "Uma Pergunta Por Dia",
+      descricao:
+        "Todos os dias criamos uma imensa quantidade de registros em celulares, redes sociais e aplicativos. No entanto, quase nunca temos o hábito de retornar a eles. Às vezes podem parecer só besteiras, mas quantos desses relatos não mostrariam nosso crescimento e nossas mudanças em todos esses anos? Uma pergunta por dia convida você a registrar suas respostas a uma variedade de questões, das mais simples às mais complicadas, como “Para onde você quer fazer sua próxima viagem?” ou “Escreva a primeira linha da sua autobiografia”. Em cada página há espaço para cinco respostas, uma por ano, ao longo de cinco anos. Com o passar do tempo, quando voltar a um dia já anotado, o dono do diário encontrará seus pensamentos anteriores, num exercício divertido e construtivo de recordar e refletir.Depois das primeiras páginas fica impossível parar, e as possibilidades são infinitas: comece agora mesmo, não importa que dia é hoje; responda sozinho, com o melhor amigo ou com um grupo inteiro de amigos; um casal pode registrar junto suas respostas, cada um em seu livro ou num livro só. Respondendo, relendo, guardando para si ou compartilhando, a brincadeira funciona como uma verdadeira cápsula do tempo – termine um ano, inicie outro e redescubra a cada página um novo eu. Ideal tanto para quem adora diários quanto para aqueles que têm dificuldade em mantê-los, pois inclui propostas de assuntos.erfeito para presentear.",
+      categoria: "autoconhecimento, desenvolvimento pessoal",
+      url_img:
+        "https://images-na.ssl-images-amazon.com/images/I/717O27bAYoL.jpg",
+      preco: "47,90",
+      total_paginas: 368,
+      ano_publicacao: "2015",
+      autor: "Potter Style",
+    },
   ];
   return (
     <div className={styles.carrinhoContainer}>
-      <h1>Carrinho</h1>
+      <h1>Meu carrinho</h1>
 
-      {livro.map((item, index) => {
+      {livro.map((livro, index) => {
         return (
           <ItemCarrinho
             key={index}
-            src={item.url_img}
-            titulo={item.titulo}
-            preco={item.preco}
+            src={livro.url_img}
+            titulo={livro.titulo}
+            autor={livro.autor}
+            preco={livro.preco}
           />
         );
       })}
