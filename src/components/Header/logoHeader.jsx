50d166d653f9cff84385d7faa48c504fd634e897
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-17 22:12:21 -0300

    Inserido nome na logo do componente Header

--- src/components/Header/logoHeader.jsx
+++ src/components/Header/logoHeader.jsx
@@ -3,16 +3,19 @@ import S from './header.module.css'
 import branco from '../../assets/logo_livraria_branco.png'
 import preto from '../../assets/logo_livraria_preto.png'
 
-export const Change = () => {
+export const Logo = () => {
     const [over, setOver] = useState(false);
     return (
         <div
             onMouseOver={() => setOver(true)}
             onMouseOut={() => setOver(false)}
+            className={S.logo}
         >
             <a href="/">
-                <img className={S.logo} src={over ? branco : preto} />
+                <img className={S.imagem_logo} src={over ? branco : preto} />
             </a>
+
+            <h1 className={S.texto_logo}> Meu livro </h1>
         </div>
     )
 }
