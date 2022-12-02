c7f8d57c1249eed9f6ae2372435f46642332ac16
Lucas Medeiros <103211800+LucasMedeiros7@users.noreply.github.com>
2022-12-02 08:28:33 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://meulivroapi.herokuapp.com/",
+  baseURL: "https://meu-livro-api.onrender.com/",
 });
 
 export default api;
